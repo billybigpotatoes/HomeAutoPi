1ddd4696c77bc191100d8fea98c9f22abfc8dcab
Dorian Logan <dorian@slug.co.uk>
2016-06-14 11:43:35 +0100

    Update camera.php

--- web/camera.php
+++ web/camera.php
@@ -21,7 +21,7 @@
 	$curl_handle=curl_init();
 	curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,1);
 	curl_setopt($curl_handle,CURLOPT_URL,$url); 
-	curl_setopt($curl_handle, CURLOPT_USERPWD, "boss:boss");
+	curl_setopt($curl_handle, CURLOPT_USERPWD, "<user>:<password>");
 	$buffer = curl_exec($curl_handle);
 	curl_close($curl_handle);
 
